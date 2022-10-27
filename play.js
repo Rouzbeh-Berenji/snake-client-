ea12bce88fb750ef13c2b4b8a8d4be2221495b55
Rouzbeh-Berenji <berenji.ro@gmail.com>
2022-10-26 23:24:10 -0400

    play.js

--- play.js
+++ play.js
@@ -1,27 +1,16 @@
 // establishes a connection with the game server
-const conn = require("./client.js");
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function () {
-  if (stdin === "\u003c"){
-process.exit ();
-  }
-  // your code here
-};
+const {conn, } = require("./client.js");
+const {setupInput, } = require("./input.js");
   // interpret incoming data as text
-  conn.setEncoding("utf8");
   console.log("Connecting ...");
+
 conn.on("connect", (msg) => {
-  // code that does something\
+  //code that does something
   conn.write ("Name: ber")
     setTimeout(() => {
     conn.write ("Move: up")
   }, 100);
   console.log(msg);
 });
+// const connect = conn();
+// setupInput(connect);
